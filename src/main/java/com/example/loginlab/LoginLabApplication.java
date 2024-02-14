f8e09aeab64e2d47e0562f966d3eb8d48190af26
Yehyeok Bang <qkddpgur318@gmail.com>
2024-02-15 00:11:22 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/example/loginlab/LoginLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loginlab/LoginLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
